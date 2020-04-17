af93f3bc02ef2e6b74863f774e38aa9bdb89fbcf
Tien Tran <tientran030197@gmail.com>
2020-04-17 20:20:34 +0700

    update my app

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('addtodo/',views.add_to_do, name= 'addtodo'),
     path('deletetodo/<int:iddelete>',views.delete_to_do, name= 'deletetodo'),
     path('completetodo/<int:idcomplete>',views.complete_to_do, name= 'completetodo'),
-    path('undo/<int:idundo>',views.undo_to_do, name= 'undo'),
-    path('edit/<int:idedit>',views.edit_to_do, name= 'edit'),
+    
 ]

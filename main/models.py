c04eff977ecec4ac970ff22b3cd84201e77caee4
Tien Tran <tientran030197@gmail.com>
2020-04-17 20:33:51 +0700

    updated model

--- main/models.py
+++ main/models.py
@@ -5,6 +5,9 @@ class Todo(models.Model):
     text =models.CharField(max_length = 255)
     added_date =models.DateTimeField()
     is_completed = models.BooleanField(default=False)
-
+    
     def __str__(self):
         return self.text
+class TaskList(models.Model):
+    title = models.CharField(max_length =100)
+    todo = models.ForeignKey(Todo,on_delete=models.CASCADE,null = True)
